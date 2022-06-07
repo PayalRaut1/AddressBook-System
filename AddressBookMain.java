d1d8e0ca32873110c3b7f1202b91417288347597
PayaLsRaut <payalraut107@gmail.com>
2022-06-07 15:32:14 +0530

    Sloved Program Using Get Set Method

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,8 @@
 package com.bridgelabz;
 
-public class AddressBookSystem
+public class AddressBookMain
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
     }
 }
